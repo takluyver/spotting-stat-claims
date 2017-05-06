c0f4e4a5735212788021fd2cb71e96e27eb300c0
hramsden <helen.ramsden2@kpmg.co.uk>
2017-05-06 16:17:02 +0100

    Added number words

--- stats_functions.py
+++ stats_functions.py
@@ -3,6 +3,7 @@ from __future__ import division, print_function
 import numpy as np
 import pandas as pd
 import re
+import json
 
 class Extractor(object):
 	"""
@@ -10,6 +11,8 @@ class Extractor(object):
 	"""
 	def __init__(self, text_in):
 		self.text = text_in
+		with open('word_numbers.json') as fp:
+			self.word_list = json.load(fp)
 
 	def extract_stats_from_text(self):
 		"""
@@ -19,6 +22,7 @@ class Extractor(object):
 		nums = self._extract_numbers()
 		nums.extend(self._extract_numberwords())
 		nums = sorted(nums, key =lambda x:x[0])
+		print([''.join(list(self.text)[n[0]:n[1]]) for n in nums])
 		return nums
 
 	def _extract_numberwords(self):
@@ -26,8 +30,25 @@ class Extractor(object):
 		Function to extract numerical related words
 		:return:
 		"""
+		all_indices = []
+		for k,v in self.word_list.items():
+			for w in v:
+				if w in self.text:
+					word_find = re.finditer(w, self.text)
+					word_inds = [(m.start(0), m.end(0)) for m in word_find]
+					all_indices.extend(word_inds)
 
-		return []
+		return all_indices
+
+	def _extract_char(self):
+		"""
+		Use regex to extract all digits in piece of text
+		:return: initial index
+		"""
+		nums = [(m.start(0), m.end(0)) for m in re.finditer(
+			r"[+-]?\d+(?:\.\d+)?", self.text)]
+
+		return nums
 
 	def _extract_numbers(self):
 		"""
@@ -36,7 +57,6 @@ class Extractor(object):
 		"""
 		nums = [(m.start(0), m.end(0)) for m in re.finditer(
 			r"[+-]?\d+(?:\.\d+)?", self.text)]
-		print([''.join(list(self.text)[n[0]:n[1]]) for n in nums])
 
 		return nums
 
@@ -44,4 +64,5 @@ if __name__ == '__main__':
 	text_in = "The hon. Gentleman makes a reasonable point: 18 per cent. is " \
 	          "still too high. However, that figure of 18 per cent. is significantly down on the percentage seven or eight years ago. The figures then were 23.7 per cent. in the Crown court, down to 13 per cent. in the latest figures, and 31 per cent. in the magistrates courts, down to 18 per cent., as I have just mentioned. We are absolutely committed to reducing those figures further, and good case management is of course part of that process."
 	em = Extractor(text_in)
-	em.extract_stats_from_text()
+	indices = em.extract_stats_from_text()
+	print(indices)
